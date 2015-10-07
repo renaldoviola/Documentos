1abdbe31d59416493ff81efa5300726def791acc
Renaldo Viola <violarenaldo@gmail.com>
2015-10-07 03:08:33 -0300

    removido datapiker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,3 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-datepicker
-//= require bootstrap-datepicker/core
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
-//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
